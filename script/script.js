767df2872bc7a8cbfdba81bf8a908a12542d49c1
Alexandre Delmer <adelmer@arsenal.arsenal.fr>
2024-01-26 16:41:08 +0100

    MAJ

--- script/script.js
+++ script/script.js
@@ -2,18 +2,18 @@
 
 let xpListTitleBtn = document.querySelectorAll(".xp_list_title_btn")
 let xpListDescription = document.querySelectorAll(".xp_list_description")
-let xpList = document.querySelectorAll(".xp_list_title")
+let xpListTitle = document.querySelectorAll(".xp_list_title")
 
 //Une boucle pour écouter chaque "+" et "-"
 for(let i = 0; i <xpListTitleBtn.length; i++){
     xpListTitleBtn[i].addEventListener('click', ()=>{
         xpListDescription[i].classList.toggle('toggle-element')
-
-        if(xpListTitleBtn[i].textContent === "+"){
-            xpListTitleBtn[i].textContent = "-"
-        } else{
-            xpListTitleBtn[i].textContent = "+"
-        }
+        xpListTitle[i].classList.toggle('marginBottom')
+        xpListTitle[i].textContent === "+" ? xpListTitleBtn[i].textContent = "-" : xpListTitleBtn[i].textContent = "+";
+        xpListTitle[i].textContent === "+" ? xpListDescription[i].classList.toggle('displayBottom') : xpListDescription[i].classList.toggle('displayTop');
+        xpListDescription[i].addEventListener('animationend', ()=> {
+            xpListDescription[i].classList.remove('displayBottom')
+            xpListDescription[i].classList.remove('displayTop')
+        })
     })
-
 }
